9c55ddc2dd19b2cbee7feb8120cc16a8c069e11d
NakorTBR <nakor_@hotmail.com>
2024-11-19 14:21:00 -0500

    Have to step away for a bit of an emergency.

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from pyramid.config import Configurator
 
 # DB error:  Not properly configured.  (does not exist).
 # TODO: Redo DB setup with better instructions.
+# Bit of an emergency, doing a push because I can't remember what I've changed.
+# I think most of it is outside the repo, but here we are.
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
