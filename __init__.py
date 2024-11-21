0288eb1f1a4d827a7b4df2aeb77190d2a907250f
NakorTBR <nakor_@hotmail.com>
2024-11-21 18:06:19 -0500

    Finally fixed my database issues.

--- __init__.py
+++ __init__.py
@@ -5,6 +5,9 @@ from pyramid.config import Configurator
 # Bit of an emergency, doing a push because I can't remember what I've changed.
 # I think most of it is outside the repo, but here we are.
 # Taking the day off.  Haven't taken a day off in 47 days.
+# I know if someone looks at these commits it will look like I've just been pretending to work,
+# but all of the work was in files that do not get commited due to security.  Everything is currently 
+# working (finally), and tommorrow I can start pushing actual code.
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
